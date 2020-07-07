d542e337f8c5fc22d5eda04fa2b37977aea27779
Christian Chang <christianchangthomaz@gmail.com>
2020-07-06 22:09:59 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/christianchang/cursomc/services/ClienteService.java
@@ -0,0 +1,27 @@
+package com.christianchang.cursomc.services;
+
+	
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.christianchang.cursomc.domain.Cliente;
+import com.christianchang.cursomc.repositories.ClienteRepository;
+import com.christianchang.cursomc.services.exceptions.ObjectNotFoundException;
+
+
+@Service
+public class ClienteService {
+ 
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente find(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		 "Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}	
+}
+	
