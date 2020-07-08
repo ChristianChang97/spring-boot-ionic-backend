74707e6ac0c747eb195ffb13566b1b09f173b3da
Christian Chang <christianchangthomaz@gmail.com>
2020-07-08 16:34:29 -0300

    ItemPedido e ItemPedidoPK

---
+++ src/main/java/com/christianchang/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,15 @@
+package com.christianchang.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.christianchang.cursomc.domain.ItemPedido;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer>{
+
+	
+	
+	
+	
+}
