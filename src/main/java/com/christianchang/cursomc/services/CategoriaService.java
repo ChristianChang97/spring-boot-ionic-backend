69a205723baaa988580515a0fa863de5fb3d7d75
Christian Chang <christianchangthomaz@gmail.com>
2020-07-09 22:26:51 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/com/christianchang/cursomc/services/CategoriaService.java
+++ src/main/java/com/christianchang/cursomc/services/CategoriaService.java
@@ -5,10 +5,12 @@ package com.christianchang.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.christianchang.cursomc.domain.Categoria;
 import com.christianchang.cursomc.repositories.CategoriaRepository;
+import com.christianchang.cursomc.services.exceptions.DataIntegrityException;
 import com.christianchang.cursomc.services.exceptions.ObjectNotFoundException;
 
 
@@ -33,5 +35,16 @@ public class CategoriaService {
 		find(obj.getId());
 		return repo.save(obj);
 	}
+	
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id); 
+		}
+		catch(DataIntegrityViolationException e){
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
+	}	
 }
 	
