f2e779c818b73313c47faf46e1c2aecb9bd4a672
Christian Chang <christianchangthomaz@gmail.com>
2020-12-17 20:39:32 -0300

    Adicionando senha a Cliente

--- src/main/java/com/christianchang/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/christianchang/cursomc/dto/ClienteNewDTO.java
@@ -27,6 +27,9 @@ public class ClienteNewDTO implements Serializable{
 	@CPF
 	private String cpfOuCnpj;
 	private Integer tipo;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
+	private String senha;
 
 	private Integer id;
 	
@@ -163,6 +166,14 @@ public class ClienteNewDTO implements Serializable{
 
 	public void setCidadeId(Integer cidadeId) {
 		this.cidadeId = cidadeId;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
 	}
 	
 }
