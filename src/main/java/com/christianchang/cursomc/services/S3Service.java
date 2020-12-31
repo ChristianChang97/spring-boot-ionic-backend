048813682ae4660d3decc227639eaee303b06f36
Christian Chang <christianchangthomaz@gmail.com>
2020-12-30 22:23:28 -0300

    Tratando excecoes adequadamente

--- src/main/java/com/christianchang/cursomc/services/S3Service.java
+++ src/main/java/com/christianchang/cursomc/services/S3Service.java
@@ -1,6 +1,5 @@
 package com.christianchang.cursomc.services;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
@@ -15,7 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.christianchang.cursomc.services.exceptions.FileException;
 
 @Service
 public class S3Service {
@@ -36,7 +35,7 @@ public class S3Service {
 			return uploadFile(is, fileName, contentType);
 		}
 		catch (IOException e) {
-			throw new RuntimeException("Erro de IO: " + e.getMessage());
+			throw new FileException("Erro de IO: " + e.getMessage());
 		}
 		
 	}
@@ -50,7 +49,7 @@ public class S3Service {
 			LOG.info("Upload finalizado");
 			return s3client.getUrl(bucketName, fileName).toURI();
 		} catch (URISyntaxException e) {
-			throw new RuntimeException("Erro ao converter URL para URI");
+			throw new FileException("Erro ao converter URL para URI");
 		}
 	}
 
