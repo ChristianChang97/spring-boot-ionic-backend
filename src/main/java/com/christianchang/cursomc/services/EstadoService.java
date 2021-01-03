ffbacbbfc19e77f0a377047f1c5b45fbb49c5b2e
Christian Chang <christianchangthomaz@gmail.com>
2021-01-03 13:26:59 -0300

    Endpoints para buscar estados e cidades

---
+++ src/main/java/com/christianchang/cursomc/services/EstadoService.java
@@ -0,0 +1,20 @@
+package com.christianchang.cursomc.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.christianchang.cursomc.domain.Estado;
+import com.christianchang.cursomc.repositories.EstadoRepository;
+
+@Service
+public class EstadoService {
+
+	@Autowired
+	private EstadoRepository repo;
+	
+	public List<Estado> findAll() {
+		return repo.findAllByOrderByNome();
+	}
+}
